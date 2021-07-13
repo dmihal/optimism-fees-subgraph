8fe9e1dc251d639806bdbc621d0b7173ae500e90
David Mihal <david@dmihal.com>
2021-07-13 16:50:52 +0100

    Create subgraph for indexing total fees

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,55 +1,25 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Contract, Approval, Transfer } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt, Address } from "@graphprotocol/graph-ts"
+import { Transfer } from "../generated/ETH/IERC20"
+import { Fee } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+let baseUnit = BigInt.fromI32(10).pow(18).toBigDecimal()
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+let treasuryAddress = Address.fromString('0x4200000000000000000000000000000000000011')
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleTransfer(event: Transfer): void {
+  if (event.params.to != treasuryAddress) {
+    return
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let entity = Fee.load('1')
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
+  if (entity == null) {
+    entity = new Fee('1')
 
-  // Entities can be written to the store with `.save()`
+    entity.totalFees = BigInt.fromI32(0).toBigDecimal()
+  }
+
+  entity.totalFees = entity.totalFees + event.params.value.divDecimal(baseUnit)
+
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.name(...)
-  // - contract.approve(...)
-  // - contract.totalSupply(...)
-  // - contract.transferFrom(...)
-  // - contract.decimals(...)
-  // - contract.balanceOf(...)
-  // - contract.symbol(...)
-  // - contract.transfer(...)
-  // - contract.allowance(...)
 }
-
-export function handleTransfer(event: Transfer): void {}
