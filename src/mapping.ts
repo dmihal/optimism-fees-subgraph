6eeda3d774cc6b9561b805acdb5ebe53b3701dc1
David Mihal <david@dmihal.com>
2021-07-13 17:24:54 +0100

    Index blocks by day

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,6 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts"
 import { Transfer } from "../generated/ETH/IERC20"
-import { Fee } from "../generated/schema"
+import { Fee, DateToBlock } from "../generated/schema"
 
 let baseUnit = BigInt.fromI32(10).pow(18).toBigDecimal()
 
@@ -22,4 +22,13 @@ export function handleTransfer(event: Transfer): void {
   entity.totalFees = entity.totalFees + event.params.value.divDecimal(baseUnit)
 
   entity.save()
+
+  let timestampRoundedDown = event.block.timestamp.toI32() / 86400 * 86400
+  let block = DateToBlock.load(timestampRoundedDown.toString())
+
+  if (block == null) {
+    block = new DateToBlock(timestampRoundedDown.toString())
+    block.blockNum = event.block.number
+    block.save()
+  }
 }
